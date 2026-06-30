ad5fd5d3e852cf98fd83d1c9968536237c420440
Ana Lysyk <ana.testing.company@gmail.com>
2026-06-30 20:26:25 -0300

    feat: add run detail page route

---
+++ app/runs/[id]/page.tsx
@@ -0,0 +1,13 @@
+import RunDetailClient from "./RunDetailClient";
+
+export const dynamic = "force-dynamic";
+
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function RunDetailPage({ params }: PageProps) {
+  return <RunDetailClient runId={params.id} />;
+}

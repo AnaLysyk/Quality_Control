385fba1874f69cfd62608a525eef19d5eea51bdb
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-01 08:58:59 -0300

    fix: reutiliza dashboard admin no dashboard geral

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,1 @@
-﻿export const dynamic = "force-dynamic";
-
-import DashboardClient from "./DashboardClient";
-import DashboardPrintShell from "./DashboardPrintShell";
-
-export default function DashboardPage() {
-  return (
-    <DashboardPrintShell>
-      <DashboardClient />
-    </DashboardPrintShell>
-  );
-}
+export { default, dynamic } from "../admin/dashboard/page";

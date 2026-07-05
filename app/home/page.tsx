68bbb61acfa607861327db6d190f0d2b76e4335d
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-05 16:42:31 -0300

    chore: carrega acabamento premium do orb da home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,5 +3,10 @@ export const dynamic = "force-dynamic";
 import HomeContent from "./HomeContent";
 
 export default function HomePage() {
-  return <HomeContent />;
+  return (
+    <>
+      <link rel="stylesheet" href="/brain-home-orb-reference.css" />
+      <HomeContent />
+    </>
+  );
 }

f599fb85b60252a6e49347ecd13d33ef28c0aa7c
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-05 15:06:18 -0300

    perf: remover warmup pesado da Home

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -1,11 +1,5 @@
 import HomeContent from "../../home/HomeContent";
-import HomeRouteWarmup from "./HomeRouteWarmup";
 
 export default function AdminHomePage() {
-  return (
-    <>
-      <HomeRouteWarmup />
-      <HomeContent />
-    </>
-  );
+  return <HomeContent />;
 }

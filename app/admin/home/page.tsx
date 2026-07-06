8b987ecdec65b32f98005934c387b82ecad63c73
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-06 09:21:46 -0300

    fix: aplicar experiencia Brain na admin home

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -4,6 +4,7 @@ import HomeRouteWarmup from "./HomeRouteWarmup";
 export default function AdminHomePage() {
   return (
     <>
+      <link rel="stylesheet" href="/brain-home-orb-reference.css" />
       <HomeRouteWarmup />
       <HomeContent />
     </>

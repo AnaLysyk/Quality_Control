824cf69c7156dbc0cc31954f7823f30df988b519
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-06 18:30:44 -0300

    fix home brain

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,7 @@
 export const dynamic = "force-dynamic";
 
-import HomeContent from "./HomeContent";
+import NewHomeContent from "../admin/home/NewHomeContent";
 
 export default function HomePage() {
-  return (
-    <>
-      <link rel="stylesheet" href="/brain-home-orb-reference.css" />
-      <HomeContent />
-    </>
-  );
+  return <NewHomeContent />;
 }

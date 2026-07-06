ee153ceb393f5a65fd6bee144449456eaa5dc3b1
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-06 11:14:52 -0300

    fix(home): switch to NewHomeContent component

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -4,6 +4,7 @@ import HomeRouteWarmup from "./HomeRouteWarmup";
 export default function AdminHomePage() {
   return (
     <>
+      {/* Mantém estilos de referência do orb */}
       <link rel="stylesheet" href="/brain-home-orb-reference.css" />
       <HomeRouteWarmup />
       <NewHomeContent />

27b9253c6a12b3ca63cb0bf76f18d4206090c189
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-06 18:32:27 -0300

    fix: incluir estilos da home brain

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,5 +3,12 @@ export const dynamic = "force-dynamic";
 import NewHomeContent from "../admin/home/NewHomeContent";
 
 export default function HomePage() {
-  return <NewHomeContent />;
+  return (
+    <>
+      <link rel="stylesheet" href="/brain-home-orb-reference.css" />
+      <link rel="stylesheet" href="/admin-home-brain-reference.css" />
+      <link rel="stylesheet" href="/admin-home-brain-footer-position.css" />
+      <NewHomeContent />
+    </>
+  );
 }

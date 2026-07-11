f533e0f0c0a3a04c538a20f6871ed301fe868b3e
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-11 18:08:00 -0300

    feat(links): add relationship management page

---
+++ app/usuarios/vinculos/page.tsx
@@ -0,0 +1,7 @@
+import RelationshipManagementClient from "./RelationshipManagementClient";
+
+export const dynamic = "force-dynamic";
+
+export default function RelationshipManagementPage() {
+  return <RelationshipManagementClient />;
+}

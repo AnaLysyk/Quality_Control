cf9e73832d7b9530c9af9665501d59b0c134897d
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-12 01:13:26 -0300

    fix(relationships): mover histórico por perfil para ícone lateral

--- app/usuarios/vinculos/RelationshipManagementWorkspace.tsx
+++ app/usuarios/vinculos/RelationshipManagementWorkspace.tsx
@@ -6,22 +6,4 @@ import { FiClock } from "react-icons/fi";
 import RelationshipHistoryByCompanyPanel from "./RelationshipHistoryByCompanyPanel";
 import RelationshipManagementClientV4 from "./RelationshipManagementClientV4";
 
-export default function RelationshipManagementWorkspace() {
-  const [historyOpen, setHistoryOpen] = useState(false);
-
-  return (
-    <div className="relationship-workspace">
-      <button
-        type="button"
-        className="relationship-company-history-launcher"
-        onClick={() => setHistoryOpen(true)}
-      >
-        <FiClock />
-        <span>Histórico por empresa</span>
-      </button>
-
-      <RelationshipManagementClientV4 />
-      <RelationshipHistoryByCompanyPanel open={historyOpen} onClose={() => setHistoryOpen(false)} />
-    </div>
-  );
-}
+export default function Relationship

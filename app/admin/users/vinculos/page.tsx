b83c1f1ea66b46e5eb0eae123ce48803107060ad
Ana Lysyk <ana.testing.company@gmail.com>
2026-07-11 22:10:10 -0300

    feat(relationships): usar cliente V3

--- app/admin/users/vinculos/page.tsx
+++ app/admin/users/vinculos/page.tsx
@@ -1,4 +1,4 @@
-import RelationshipManagementClientV2 from "@/usuarios/vinculos/RelationshipManagementClientV2";
+import RelationshipManagementClientV3 from "@/usuarios/vinculos/RelationshipManagementClientV3";
 
 export const dynamic = "force-dynamic";
 
@@ -8,7 +8,7 @@ export default function RelationshipManagementAdminPage() {
       <link rel="stylesheet" href="/relationship-management.css" />
       <link rel="stylesheet" href="/relationship-management-context.css" />
       <link rel="stylesheet" href="/relationship-management-leadership.css" />
-      <RelationshipManagementClientV2 />
+      <RelationshipManagementClientV3 />
     </>
   );
 }
